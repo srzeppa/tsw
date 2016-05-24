cb479530d21fbcca82f5e5e58bd06a6ad9e564df
srzeppa <srzeppa@sigma.ug.edu.pl>
2016-05-24 20:28:23 +0200

    added admin view

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ var express = require('express');
 var router = express.Router();
 
 var isAuthenticated = function (req, res, next) {
+    console.log("tutajjjjjjjjjjjjjjjjjjjjjjjjjj222222222222222222222");
 	// if user is authenticated in the session, call the next() to call the next request handler 
 	// Passport adds this method to request object. A middleware is allowed to add properties to
 	// request and response objects
-	if (req.isAuthenticated())
+	if (req.isAuthenticated()){
+        console.log("autentykacja ok!");
 		return next();
+    }
 	// if the user is not authenticated then redirect him to the login page
 	res.redirect('/');
 };
@@ -20,7 +23,7 @@ module.exports = function(passport){
     	// Display the Login page with any flash message, if any
 		res.render('index', { message: req.flash('message') });
 	});
-
+    
 	/* Handle Login POST */
 	router.post('/login', passport.authenticate('login', {
 		successRedirect: '/referee',
