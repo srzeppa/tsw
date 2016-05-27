20112976d03eb7a0ffbc5881a8f7d7b45a1516de
srzeppa <srzeppa@sigma.ug.edu.pl>
2016-05-27 17:09:29 +0200

    socket.io propably works. todo: tests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* jshint node: true */
 
+var http = require('http');
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
@@ -9,7 +10,17 @@ var bodyParser = require('body-parser');
 
 // Configuring Passport
 var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var expressSession = require('express-session');
+
+var socketIo = require('socket.io');
+var passportSocketIo = require('passport.socketio');
+var redisStore = require('connect-redis')(expressSession);
+var client  = require('redis').createClient();
+var sessionStore = new redisStore();
+
+var sessionSecret = 'wielkiSekret44';
+var sessionKey = 'express.sid';
 
 var dbConfig = require('./db');
 var mongoose = require('mongoose');
@@ -31,9 +42,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use(expressSession({
-    secret: 'mySecretKey',
     resave: true,
-    saveUninitialized: false
+    saveUninitialized: true,
+    key: sessionKey,
+    secret: sessionSecret,
+    store: sessionStore
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -72,5 +85,47 @@ if (app.get('env') === 'development') {
         });
     });
 }
+
+
+var server = http.createServer(app);
+var sio = socketIo.listen(server);
+
+var onAuthorizeSuccess = function (data, accept) {
+    console.log('Udane połączenie z socket.io');
+    accept(null, true);
+};
+
+var onAuthorizeFail = function (data, message, error, accept) {
+    if (error) {
+        throw new Error(message);
+    }
+    console.log('Nieudane połączenie z socket.io:', message);
+    accept(null, false);
+};
+
+sio.use(passportSocketIo.authorize({
+  cookieParser: cookieParser,       // the same cookieParser middleware as registered in express
+  key:          sessionKey,         // the name of the cookie storing express/connect session_id
+  secret:       sessionSecret,      // the session_secret used to parse the cookie
+  store:        sessionStore,       // sessionstore – should not be memorystore!
+  success:      onAuthorizeSuccess, // *optional* callback on success
+  fail:         onAuthorizeFail     // *optional* callback on fail/error
+}));
+
+
+sio.set('log level', 2); // 3 == DEBUG, 2 == INFO, 1 == WARN, 0 == ERROR
+
+sio.sockets.on('connection', function (socket) {
+    socket.emit('news', {
+        ahoj: 'od serwera'
+    });
+    socket.on('reply', function (data) {
+        console.log(data);
+    });
+});
+
+server.listen(3001, function () {
+    console.log('Serwer pod adresem http://localhost:3001/');
+});
 
 module.exports = app;
