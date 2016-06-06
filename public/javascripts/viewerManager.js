3895076ed0584c8c054623cf0d5238f9382f3119
srzeppa <srzeppa@sigma.ug.edu.pl>
2016-06-06 13:37:37 +0200

    viewers view adding livescore to good competition

--- public/javascripts/viewerManager.js
+++ public/javascripts/viewerManager.js
@@ -12,17 +12,34 @@ $( document ).ready(function() {
         url: '/viewer/getAllResults/',
         dataType: 'json',
         success: function(e){
+            console.log('e');
             console.log(e);
-            var $table = $( "<table id=\"competitionsViewTable\" class='table table-hover'><thead><tr><th>Name</th><th>Overall</th></tr></thead></table>" );
             var $tbody = $("<tbody></tbody>");
             for(let i = 0; i < e.length; i ++){
-                var $line = $( "<tr></tr>" );
-                $line.append( $( "<td></td>" ).html( e[i].horse.name ) );
-                $line.append( $( "<td></td>" ).html( e[i].overall ) );
-                $tbody.append( $line );
-                $table.append( $tbody );
+                console.log(e[i].competition._id);
+                if($('#' + e[i].competition._id).length){
+                    console.log('e[i].competition._id).length');
+                    console.log('i');
+                    console.log(i);
+                    var $line = $( "<tr></tr>" );
+                    $line.append( $( "<td></td>" ).html( e[i].horse.name ) );
+                    $line.append( $( "<td></td>" ).html( e[i].overall ) );
+                    $line.appendTo( $( "#" + e[i].competition._id));
+                } else {
+                    console.log('else');
+                    console.log('i');
+                    console.log(i);
+                    var $title = $( "<h1>" + e[i].competition.name + "</h1>");
+                    $title.appendTo( $( "#results" ) );
+                    var $table = $( "<table id=\"" + e[i].competition._id + "\" class='table table-hover'><thead><tr><th>Name</th><th>Overall</th></tr></thead></table>" );
+                    $table.appendTo( $( "#results" ) );
+                    var $linee = $( "<tr></tr>" );
+                    $linee.append( $( "<td></td>" ).html( e[i].horse.name ) );
+                    $linee.append( $( "<td></td>" ).html( e[i].overall ) );
+//                    $tbody.append( $linee );
+                    $linee.appendTo( $( "#" + e[i].competition._id));
+                }
             }
-            $table.appendTo( $( "#results" ) );
         }
     });
     
@@ -38,7 +55,7 @@ $( document ).ready(function() {
             success: function(e){
                 $line.append( $( "<td></td>" ).html( e.name ) );
                 $line.append( $( "<td></td>" ).html( data.overall ) );
-                $line.appendTo( $( "#competitionsViewTable" ) );
+                $line.appendTo( $( "#" + data.competition ) );
             }
         });
         
